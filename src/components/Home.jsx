dd8dfba58eaeeb808ad4a17d350e3fe5c8539358
Dewa Sembiring <dewasembiring1@gmail.com>
2022-07-16 15:49:54 +0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-orange-600'>Hi, my name is</p>
                 <h1 className='text-4xl md:text-7xl font-bold text-[#ccd6f6]'>
-                    Dewa Sembirings
+                    Dewa Sembiring
                 </h1>
                 <h2 className='text-4xl md:text-7xl font-bold text-[#8892b0]'>
                     I'm a Web Developer
